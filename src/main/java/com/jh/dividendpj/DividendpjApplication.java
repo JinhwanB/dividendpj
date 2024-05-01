74691fe2ff0c996c9f91a493f31bb0b85ec55107
jinhwan Bea <paewhal20@gmail.com>
2024-05-01 14:11:22 +0900

    Update DividendpjApplication.java

--- src/main/java/com/jh/dividendpj/DividendpjApplication.java
+++ src/main/java/com/jh/dividendpj/DividendpjApplication.java
@@ -2,12 +2,14 @@ package com.jh.dividendpj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class DividendpjApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DividendpjApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DividendpjApplication.class, args);
+    }
 
 }
