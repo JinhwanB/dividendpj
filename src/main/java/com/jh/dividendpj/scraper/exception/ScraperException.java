fc71af1e7c024de3f336d44203c545db5857e9ad
jinhwan Bea <paewhal20@gmail.com>
2024-05-02 08:28:12 +0900

    커스텀 exception 생성

---
+++ src/main/java/com/jh/dividendpj/scraper/exception/ScraperException.java
@@ -0,0 +1,14 @@
+package com.jh.dividendpj.scraper.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ScraperException extends RuntimeException {
+    private String message;
+    private ScraperErrorCode scraperErrorCode;
+
+    public ScraperException(ScraperErrorCode scraperErrorCode, String message) {
+        super(message);
+        this.scraperErrorCode = scraperErrorCode;
+    }
+}
