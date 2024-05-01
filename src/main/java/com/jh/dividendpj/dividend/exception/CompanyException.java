dbad3ef171cdd232ff5bf74e6f63544b238a34cb
jinhwan Bea <paewhal20@gmail.com>
2024-05-01 14:24:42 +0900

    회사 커스텀 예외 생성

---
+++ src/main/java/com/jh/dividendpj/dividend/exception/CompanyException.java
@@ -0,0 +1,11 @@
+package com.jh.dividendpj.dividend.exception;
+
+public class CompanyException extends RuntimeException {
+    public CompanyException() {
+        super();
+    }
+
+    public CompanyException(String message) {
+        super(message);
+    }
+}
