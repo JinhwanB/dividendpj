6b105f41ac9c3b3a9258b1d71f44963771d5cd7d
jinhwan Bea <paewhal20@gmail.com>
2024-05-01 14:54:25 +0900

    회사 조회 기능 서비스 추가

--- src/main/java/com/jh/dividendpj/company/service/CompanyService.java
+++ src/main/java/com/jh/dividendpj/company/service/CompanyService.java
@@ -1,5 +1,9 @@
 package com.jh.dividendpj.company.service;
 
+import com.jh.dividendpj.company.domain.Company;
+import com.jh.dividendpj.company.dto.CreateCompanyDto;
+import com.jh.dividendpj.company.exception.CompanyErrorCode;
+import com.jh.dividendpj.company.exception.CompanyException;
 import com.jh.dividendpj.company.repository.CompanyRepository;
 import com.jh.dividendpj.scraper.YahooScraper;
 import lombok.RequiredArgsConstructor;
@@ -15,5 +19,24 @@ public class CompanyService {
     private final CompanyRepository companyRepository;
     private final YahooScraper yahooScraper;
 
+    public Company createCompany(CreateCompanyDto.Request request) {
+        String ticker = request.getTicker();
+        Company company = companyRepository.findByTickerAndDelDate(ticker, null).orElse(null);
+        if (company != null) {
+            throw new CompanyException(CompanyErrorCode.ALREADY_EXIST_COMPANY.getMessage());
+        }
+        company = yahooScraper.getCompany(ticker);
+        return companyRepository.save(company);
+    }
 
+    /**
+     * 회사 조회
+     *
+     * @param companyName 회사 이름
+     * @return 조회된 회사
+     */
+    @Transactional(readOnly = true)
+    public Company getCompany(String companyName) {
+        return companyRepository.findByNameAndDelDate(companyName, null).orElseThrow(() -> new CompanyException(CompanyErrorCode.NOT_FOUND_NAME.getMessage()));
+    }
 }
