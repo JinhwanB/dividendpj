e7ce49ac5904ed6397f22066e45ac3b0e4f5207b
jinhwan Bea <paewhal20@gmail.com>
2024-05-01 15:38:52 +0900

    Update CompanyRepository.java

--- src/main/java/com/jh/dividendpj/company/repository/CompanyRepository.java
+++ src/main/java/com/jh/dividendpj/company/repository/CompanyRepository.java
@@ -4,15 +4,14 @@ import com.jh.dividendpj.company.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-    Optional<Company> findByTickerAndDelDate(String ticker, LocalDateTime delDate);
+    Optional<Company> findByTicker(String ticker);
 
-    List<Company> findAllByTickerAndDelDate(String ticker, LocalDateTime delDate);
+    List<Company> findAllByTicker(String ticker);
 
-    Optional<Company> findByNameAndDelDate(String name, LocalDateTime delDate);
+    Optional<Company> findByName(String name);
 }
