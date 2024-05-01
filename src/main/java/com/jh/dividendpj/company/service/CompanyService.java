3b221e3f0f5f4178313819397537ff1e64218fd4
jinhwan Bea <paewhal20@gmail.com>
2024-05-01 14:20:23 +0900

    Update CompanyService.java

--- src/main/java/com/jh/dividendpj/company/service/CompanyService.java
+++ src/main/java/com/jh/dividendpj/company/service/CompanyService.java
@@ -5,13 +5,15 @@ import com.jh.dividendpj.scraper.YahooScraper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 public class CompanyService {
     private final CompanyRepository companyRepository;
     private final YahooScraper yahooScraper;
 
-    
+
 }
