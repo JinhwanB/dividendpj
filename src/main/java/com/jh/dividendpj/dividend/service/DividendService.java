ad92eb44e98a496e6e2d007deefddbc887f09fb0
jinhwan Bea <paewhal20@gmail.com>
2024-05-01 15:03:19 +0900

    스크랩하여 배당금 정보 가져오기 기능 서비스 추가

--- src/main/java/com/jh/dividendpj/dividend/service/DividendService.java
+++ src/main/java/com/jh/dividendpj/dividend/service/DividendService.java
@@ -1,11 +1,16 @@
 package com.jh.dividendpj.dividend.service;
 
+import com.jh.dividendpj.company.domain.Company;
+import com.jh.dividendpj.company.service.CompanyService;
+import com.jh.dividendpj.dividend.domain.Dividend;
 import com.jh.dividendpj.dividend.repository.DividendRepository;
 import com.jh.dividendpj.scraper.YahooScraper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -14,4 +19,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class DividendService {
     private final DividendRepository dividendRepository;
     private final YahooScraper yahooScraper;
+    private final CompanyService companyService;
+
+    public List<Dividend> getDividendInfo(String companyName) {
+        Company company = companyService.getCompany(companyName);
+        return yahooScraper.getDividendList(company);
+    }
 }
