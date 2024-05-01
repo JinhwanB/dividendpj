f6fcf5c9e9bd775aecbb12590734516a3f15b80b
jinhwan Bea <paewhal20@gmail.com>
2024-05-01 15:05:16 +0900

    Update Company.java

--- src/main/java/com/jh/dividendpj/company/domain/Company.java
+++ src/main/java/com/jh/dividendpj/company/domain/Company.java
@@ -1,11 +1,9 @@
 package com.jh.dividendpj.company.domain;
 
-import com.jh.dividendpj.dividend.domain.Dividend;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Getter
@@ -18,9 +16,6 @@ public class Company {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
-    private List<Dividend> devidendList;
 
     @Column(nullable = false, unique = true)
     private String name;
