47302745e47912d36ce59f23a809e31bc9ad9398
jinhwan Bea <paewhal20@gmail.com>
2024-05-01 10:57:31 +0900

    first init

---
+++ src/main/java/com/jh/dividendpj/DividendpjApplication.java
@@ -0,0 +1,13 @@
+package com.jh.dividendpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DividendpjApplication.class, args);
+	}
+
+}
