79c4d92bf2084be10f2c8b5d2e273de49072bdb1
jinhwan Bea <paewhal20@gmail.com>
2024-05-01 17:43:40 +0900

    회사 삭제 컨트롤러 추가

--- src/main/java/com/jh/dividendpj/company/controller/CompanyController.java
+++ src/main/java/com/jh/dividendpj/company/controller/CompanyController.java
@@ -3,37 +3,50 @@ package com.jh.dividendpj.company.controller;
 import com.jh.dividendpj.company.domain.Company;
 import com.jh.dividendpj.company.dto.CreateCompanyDto;
 import com.jh.dividendpj.company.service.CompanyService;
+import com.jh.dividendpj.global.GlobalApiResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/companys")
+@RequestMapping("/company")
 @RequiredArgsConstructor
 @Slf4j
 @Validated
 public class CompanyController {
     private final CompanyService companyService;
 
-    @PostMapping("/company")
-    public ResponseEntity<CreateCompanyDto.Response> test(@Valid @RequestBody CreateCompanyDto.Request request) {
+    // 회사 생성
+    @PostMapping
+    public ResponseEntity<GlobalApiResponse> test(@Valid @RequestBody CreateCompanyDto.Request request) {
         log.info("입력받은 ticker={}", request.getTicker());
 
         Company company = companyService.createCompany(request);
-        return ResponseEntity.ok(company.toCreateResponseDto());
+        List<CreateCompanyDto.Response> list = new ArrayList<>(List.of(company.toCreateResponseDto()));
+        return ResponseEntity.ok(GlobalApiResponse.toGlobalApiResponse(list));
+    }
+
+    // 회사 삭제
+    @DeleteMapping("/{ticker}")
+    public ResponseEntity<GlobalApiResponse> testDelete(@PathVariable @NotBlank(message = "삭제할 ticker를 입력해주세요.") @Size(min = 1, message = "ticker는 최소 1글자 이상입니다.") String ticker) {
+        companyService.deleteCompany(ticker);
+        GlobalApiResponse response = GlobalApiResponse.builder()
+                .message("성공")
+                .status(200)
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/company")
     public Company testGet(@RequestParam String companyName) {
         return companyService.getCompany(companyName);
-    }
-
-    @DeleteMapping("/company")
-    public String testDelete(@RequestParam String ticker) {
-        companyService.deleteCompany(ticker);
-        return "성공";
     }
 }
