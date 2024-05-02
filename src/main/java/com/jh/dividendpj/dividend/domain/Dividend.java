9d7266b94e8cc9f982940578fa0a15c233e39d44
jinhwan Bea <paewhal20@gmail.com>
2024-05-02 21:41:49 +0900

    LocalDateTime 직렬화, 역직렬화 설정 추가

--- src/main/java/com/jh/dividendpj/dividend/domain/Dividend.java
+++ src/main/java/com/jh/dividendpj/dividend/domain/Dividend.java
@@ -1,5 +1,9 @@
 package com.jh.dividendpj.dividend.domain;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.jh.dividendpj.company.domain.Company;
 import com.jh.dividendpj.dividend.dto.JoinDividendDto;
 import jakarta.persistence.*;
@@ -37,6 +41,8 @@ public class Dividend {
     private String dividend;
 
     @Column
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime delDate;
 
     // Dividend -> JoinDividendDto.Response
