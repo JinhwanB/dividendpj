d4f9b7a404d7ed496881bb5643fd1f7a2f3e3907
jinhwan Bea <paewhal20@gmail.com>
2024-05-03 10:54:12 +0900

    존재하지 않는 아이디 에러코드 추가

--- src/main/java/com/jh/dividendpj/member/exception/MemberErrorCode.java
+++ src/main/java/com/jh/dividendpj/member/exception/MemberErrorCode.java
@@ -6,8 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MemberErrorCode {
+    NOT_FOUND_USERNAME(404, "존재하지 않는 아이디입니다."),
     ALREADY_EXIST_USERNAME(400, "이미 사용중인 아이디입니다.");
-    
+
     private int status;
     private String message;
 }
