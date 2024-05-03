4514a9023bf32725ceba03247f4a51c524c1f681
jinhwan Bea <paewhal20@gmail.com>
2024-05-03 12:43:40 +0900

    Update SecurityConfiguration.java

--- src/main/java/com/jh/dividendpj/auth/SecurityConfiguration.java
+++ src/main/java/com/jh/dividendpj/auth/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -19,32 +20,34 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+
     private final JwtAuthenticationFilter authenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**")
+                        .disable())
 //                .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeRequest ->
                 {
-                    AntPathRequestMatcher.antMatcher("/h2-console/**");
                     authorizeRequest
                             .requestMatchers(
                                     new AntPathRequestMatcher("/**/signin"),
-                                    new AntPathRequestMatcher("/**/signup")
+                                    new AntPathRequestMatcher("/**/signup"),
+                                    new AntPathRequestMatcher("/h2-console/**")
                             ).permitAll();
                 })
-                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
-//                .headers(
-//                        headersConfigurer ->
-//                                headersConfigurer.frameOptions(
-//                                        HeadersConfigurer.FrameOptionsConfig::sameOrigin
-//                                )
-//                );
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .headers(
+                        headersConfigurer ->
+                                headersConfigurer.frameOptions(
+                                        HeadersConfigurer.FrameOptionsConfig::disable
+                                )
+                );
         return http.build();
     }
 }
