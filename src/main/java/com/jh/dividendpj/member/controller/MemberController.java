532b5966306b272858d23b13ca98d87b3120122c
jinhwan Bea <paewhal20@gmail.com>
2024-05-03 10:59:54 +0900

    Update MemberController.java

--- src/main/java/com/jh/dividendpj/member/controller/MemberController.java
+++ src/main/java/com/jh/dividendpj/member/controller/MemberController.java
@@ -2,7 +2,7 @@ package com.jh.dividendpj.member.controller;
 
 import com.jh.dividendpj.auth.TokenProvider;
 import com.jh.dividendpj.member.domain.Member;
-import com.jh.dividendpj.member.dto.Auth;
+import com.jh.dividendpj.member.dto.MemberAuthDto;
 import com.jh.dividendpj.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,14 +22,14 @@ public class MemberController {
 
     // 회원가입을 위한 api
     @PostMapping("/signup")
-    public ResponseEntity<?> signUp(@RequestBody Auth.SignUp request) {
+    public ResponseEntity<?> signUp(@RequestBody MemberAuthDto.SignUp request) {
         Member register = memberService.register(request);
         return ResponseEntity.ok(register);
     }
 
     // 로그인을 위한 api
     @PostMapping("/signin")
-    public ResponseEntity<?> signIn(@RequestBody Auth.SignIn request) {
+    public ResponseEntity<?> signIn(@RequestBody MemberAuthDto.SignIn request) {
         Member authenticate = memberService.authenticate(request);
         String token = tokenProvider.generateToken(authenticate.getUsername(), authenticate.getRoles());
         return ResponseEntity.ok(token);
