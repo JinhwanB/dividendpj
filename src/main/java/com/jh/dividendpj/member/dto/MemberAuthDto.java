0016c59de33263ac992b7d118e3b519a236b939d
jinhwan Bea <paewhal20@gmail.com>
2024-05-06 12:10:50 +0900

    response 추가

--- src/main/java/com/jh/dividendpj/member/dto/MemberAuthDto.java
+++ src/main/java/com/jh/dividendpj/member/dto/MemberAuthDto.java
@@ -1,6 +1,7 @@
 package com.jh.dividendpj.member.dto;
 
 import com.jh.dividendpj.member.domain.Member;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.util.List;
@@ -13,7 +14,10 @@ public class MemberAuthDto {
     @Builder(toBuilder = true)
     @ToString
     public static class SignIn {
+        @NotBlank(message = "아이디를 입력해주세요.")
         private String username;
+
+        @NotBlank(message = "패스워드를 입력해주세요.")
         private String password;
     }
 
@@ -24,8 +28,12 @@ public class MemberAuthDto {
     @Builder(toBuilder = true)
     @ToString
     public static class SignUp {
+        @NotBlank(message = "아이디를 입력해주세요.")
         private String username;
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
         private String password;
+
         private List<String> roles;
 
         public Member toEntity() {
@@ -35,5 +43,15 @@ public class MemberAuthDto {
                     .roles(roles)
                     .build();
         }
+    }
+
+    @Getter
+    @Setter // 테스트용
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder(toBuilder = true)
+    @ToString
+    public static class Response {
+        private String username;
     }
 }
