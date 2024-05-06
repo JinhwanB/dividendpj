2afcfabecb6935abd68df8be72e905381eec3805
jinhwan Bea <paewhal20@gmail.com>
2024-05-06 11:31:05 +0900

    Update CompanyErrorCode.java

--- src/main/java/com/jh/dividendpj/company/exception/CompanyErrorCode.java
+++ src/main/java/com/jh/dividendpj/company/exception/CompanyErrorCode.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CompanyErrorCode {
     ALREADY_EXIST_COMPANY(400, "이미 데이터베이스에 포함되어있는 회사입니다."),
+    EMPTY_TICKER(400, "삭제할 ticker를 입력해주세요."),
+    EMPTY_COMPANY_NAME(400, "조회할 companyName을 입력해주세요."),
     NOT_FOUND_NAME(400, "존재하지 않는 회사입니다."),
     NOT_FOUND_TICKER(400, "존재하지 않는 ticker입니다.");
 
