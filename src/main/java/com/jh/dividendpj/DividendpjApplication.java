c29cc6a9cfe4cd97886e21953f28d532f61dc6c3
jinhwan Bea <paewhal20@gmail.com>
2024-05-08 15:38:33 +0900

    Update: 어노테이션 추가
    Batch 사용 활성화를 위한 어노테이션 추가

--- src/main/java/com/jh/dividendpj/DividendpjApplication.java
+++ src/main/java/com/jh/dividendpj/DividendpjApplication.java
@@ -1,5 +1,6 @@
 package com.jh.dividendpj;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableScheduling
 @EnableCaching
+@EnableBatchProcessing
 public class DividendpjApplication {
 
     public static void main(String[] args) {
