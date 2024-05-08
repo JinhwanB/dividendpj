7dc154fa470cd4702ddf0290fbf28c4b032c4b9f
jinhwan Bea <paewhal20@gmail.com>
2024-05-08 16:08:53 +0900

    Chore: 어노테이션 제거
    필요 없는 어노테이션 제거

--- src/main/java/com/jh/dividendpj/DividendpjApplication.java
+++ src/main/java/com/jh/dividendpj/DividendpjApplication.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableScheduling
 @EnableCaching
-@EnableBatchProcessing
 public class DividendpjApplication {
 
     public static void main(String[] args) {
