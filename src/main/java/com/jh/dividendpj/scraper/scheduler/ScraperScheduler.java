1b7e0f89935a3404108f0188afb00da67629e92f
jinhwan Bea <paewhal20@gmail.com>
2024-05-10 11:45:57 +0900

    Chore: 주석 코드 제거

--- src/main/java/com/jh/dividendpj/scraper/scheduler/ScraperScheduler.java
+++ src/main/java/com/jh/dividendpj/scraper/scheduler/ScraperScheduler.java
@@ -46,30 +46,5 @@ public class ScraperScheduler {
             log.error("스크랩 스케줄 도중 예외 발생!! -> {}", e.getMessage());
             throw new RuntimeException(e);
         }
-//        log.info("스크래핑 스케줄 시작");
-//        // todo: 회사가 많아질 경우 시간이 굉장히 오래 걸릴 가능성 있음! spring batch에 대해 알아보기!
-//        List<Company> companyList = companyRepository.findAll();
-//        if (!companyList.isEmpty()) {
-//            for (Company company : companyList) {
-//                log.info("스크래핑 스케줄 회사 이름 : {}", company.getName());
-//                List<Dividend> dividendList = yahooScraper.getDividendList(company);
-//                dividendList.stream()
-//                        .map(e -> e.toBuilder().company(company).build())
-//                        .forEach(e -> {
-//                            boolean isExist = dividendRepository.existsByCompanyAndDate(e.getCompany(), e.getDate());
-//                            if (!isExist) {
-//                                dividendRepository.save(e);
-//                            }
-//                        });
-//
-//                try {
-//                    Thread.sleep(3000); // 연속적으로 스크래핑 대상 사이트 서버에 요청을 날리지 않도록 일시정지
-//                } catch (InterruptedException e) {
-//                    log.error("스크래핑 스케줄 스레드 에러 = {}", e.getMessage());
-//                    Thread.currentThread().interrupt();
-//                }
-//            }
-//        }
-//        log.info("스크래핑 스케줄 정상 종료");
     }
 }
