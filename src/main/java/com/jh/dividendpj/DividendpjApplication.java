0d954b2205db8b2bb70fff0d4d59c8edafe4b16b
jinhwan Bea <paewhal20@gmail.com>
2024-05-10 09:12:56 +0900

    Chore: 사용하지 않는 어노테이션 제거

--- src/main/java/com/jh/dividendpj/DividendpjApplication.java
+++ src/main/java/com/jh/dividendpj/DividendpjApplication.java
@@ -1,6 +1,5 @@
 package com.jh.dividendpj;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
